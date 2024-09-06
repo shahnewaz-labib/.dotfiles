b87c6e52d288f8afc272fd5adbfbf3381fbf1ba6
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-09-06 18:16:58 +0600

    updated scripts

--- scripts/.scripts/launch-polybar-on-every-monitor.sh
+++ scripts/.scripts/launch-polybar-on-every-monitor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 killall -q polybar
 
-if type "xrandr"; then
+if type "xrandr" >/dev/null 2>&1; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
+    MONITOR=$m polybar --reload mybar >/dev/null 2>&1 &
   done
 else
   polybar --reload mybar &
