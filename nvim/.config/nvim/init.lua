23c80dfc46e6f0bd9371b69ba72c66643b595f14
shahnewaz-labib <shahnewaz8080@gmail.com>
2024-11-07 04:20:06 +0600

    nvim: keymap dump

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,12 @@ require 'custom.autocommands'
 
 require('lazy').setup({
   'tpope/vim-sleuth',
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
   'github/copilot.vim',
 
   require 'custom.autocompletion',
@@ -43,3 +48,35 @@ require('lazy').setup({
     },
   },
 })
+
+-- Don't move harpoon from here
+local harpoon = require 'harpoon'
+harpoon:setup()
+
+vim.keymap.set('n', '<leader>a', function()
+  harpoon:list():append()
+end)
+vim.keymap.set('n', '<C-e>', function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+-- vim.keymap.set('n', '<C-t>', function()
+--   harpoon:list():select(1)
+-- end)
+-- vim.keymap.set('n', '<C-h>', function()
+--   harpoon:list():select(2)
+-- end)
+-- vim.keymap.set('n', '<C-n>', function()
+--   harpoon:list():select(3)
+-- end)
+-- vim.keymap.set('n', '<C-s>', function()
+--   harpoon:list():select(4)
+-- end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set('n', '<C-S-P>', function()
+  harpoon:list():prev()
+end)
+vim.keymap.set('n', '<C-S-N>', function()
+  harpoon:list():next()
+end)
--- nvim/.config/nvim/lua/custom/lsp.lua
+++ nvim/.config/nvim/lua/custom/lsp.lua
@@ -146,8 +146,8 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        clangd = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -156,8 +156,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
 
         lua_ls = {
           -- cmd = {...},
