48b69d3d15dd931d6bd6bcbb510284453b16a23c
shahnewaz-labib <shahnewaz8080@gmail.com>
2024-11-23 16:37:43 +0600

    nvim: added py things, hyprland: added waybar toggle, waybar: reordering modules

--- nvim/.config/nvim/lua/custom/lsp.lua
+++ nvim/.config/nvim/lua/custom/lsp.lua
@@ -171,6 +171,7 @@ return {
         clangd = {},
         gopls = {},
         prismals = {},
+        -- pylsp = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
