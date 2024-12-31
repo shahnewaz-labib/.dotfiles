fbc210ea41f5875597b96897cea648072ab35cba
shahnewaz-labib <shahnewaz8080@gmail.com>
2024-12-31 20:05:37 +0600

    update: nvim revamp

--- nvim/.config/nvim/lua/custom/autocommands.lua
+++ nvim/.config/nvim/lua/custom/autocommands.lua
@@ -27,3 +27,19 @@ vim.notify = function(msg, ...)
 
   notify(msg, ...)
 end
+
+local peek = require 'peek'
+
+vim.api.nvim_create_user_command('PeekOpen', function()
+  if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+    vim.fn.system 'i3-msg split horizontal'
+    peek.open()
+  end
+end, {})
+
+vim.api.nvim_create_user_command('PeekClose', function()
+  if peek.is_open() then
+    peek.close()
+    vim.fn.system 'i3-msg move left'
+  end
+end, {})
--- nvim/.config/nvim/lua/custom/lsp.lua
+++ nvim/.config/nvim/lua/custom/lsp.lua
@@ -171,9 +171,8 @@ return {
         clangd = {},
         gopls = {},
         prismals = {},
-        -- pylsp = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -211,6 +210,17 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'rust-analyzer',
+        -- 'clangd',
+        'delve',
+        -- 'gopls',
+        -- 'hadolint',
+        'lua-language-server',
+        'markdownlint',
+        -- 'prisma-language-server',
+        'pyright',
+        'ruff',
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
