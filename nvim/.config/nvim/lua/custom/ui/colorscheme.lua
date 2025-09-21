8247e126931e2ea224258f5b360b25f37f54c05b
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2025-09-22 03:59:36 +0600

    update: removed themes

--- nvim/.config/nvim/lua/custom/ui/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/ui/colorscheme.lua
@@ -1,25 +1,25 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    opts = { style = 'moon' },
-    init = function()
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    -- init = function()
-    --   vim.cmd.colorscheme 'kanagawa'
-    -- end,
-  },
-  { 'sainnhe/gruvbox-material' },
-  { 'morhetz/gruvbox' },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000,
+  --   opts = { style = 'moon' },
+  --   init = function()
+  --     -- vim.cmd.colorscheme 'tokyonight-night'
+  --     -- vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   -- init = function()
+  --   --   vim.cmd.colorscheme 'kanagawa'
+  --   -- end,
+  -- },
+  -- { 'sainnhe/gruvbox-material' },
+  -- { 'morhetz/gruvbox' },
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
@@ -29,16 +29,16 @@ return {
       vim.api.nvim_set_hl(0, "SignColumn", { bg = "none"})
     end,
   },
-  {
-    'dgox16/oldworld.nvim',
-    lazy = false,
-    priority = 1000,
-    styley = {
-      booleans = { italic = true, bold = true },
-    },
-    init = function()
-      -- vim.cmd.colorscheme 'oldworld'
-      -- vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- {
+  --   'dgox16/oldworld.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   styley = {
+  --     booleans = { italic = true, bold = true },
+  --   },
+  --   init = function()
+  --     -- vim.cmd.colorscheme 'oldworld'
+  --     -- vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 }
